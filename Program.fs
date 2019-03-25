1582c6d422f5c78814e1e1561970667ad8d2b58a
Vegard Løkken <vegard@loekken.org>
2019-03-25 22:27:14 +0100

    Make calculator work with stdin
    An extension to allow piping from other programs.

--- Program.fs
+++ Program.fs
@@ -3,5 +3,10 @@ open Calculator
 
 [<EntryPoint>]
 let main argv =
-    Calculator.parse (argv |> String.concat "")
+    let input = match argv.Length with
+                | 0 -> Seq.initInfinite (fun _ -> Console.In.ReadLine())
+                    |> Seq.takeWhile (fun line -> line <> null)
+                | _ -> argv |> (String.concat "") |> Seq.singleton
+
+    input |> Seq.iter Calculator.parse
     0
