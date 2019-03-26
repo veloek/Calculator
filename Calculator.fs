6dca1f58a73167427bde55ac0c230835daaab264
Vegard Løkken <vegard@loekken.org>
2019-03-26 16:29:39 +0100

    Refactor to contain operator implementation
    It feels so much cleaner to have the operator parsers close to how
    they're implemented. Now it should be pretty straight forward to add
    new operators to the list.

--- Calculator.fs
+++ Calculator.fs
@@ -1,29 +1,19 @@
 module Calculator
 
-open System
 open FParsec
 
 let ws = spaces
 let braced p = (pchar '(') >>. p .>> (pchar ')')
 
-let add = pchar '+'
-let sub = pchar '-'
-let mul = pchar '*'
-let div = pchar '/'
+let add = pchar '+' |>> (fun _ a b -> a + b)
+let sub = pchar '-' |>> (fun _ a b -> a - b)
+let mul = pchar '*' |>> (fun _ a b -> a * b)
+let div = pchar '/' |>> (fun _ a b -> a / b)
 
-let operation = (add <|> sub <|> mul <|> div)
-
+let pOperator = (add <|> sub <|> mul <|> div)
 let pOperand, opRef = createParserForwardedToRef()
-
-let pExpr = pipe2 pOperand (many ((operation .>> ws) .>>. pOperand)) (fun start ops ->
-    ops |> List.fold (fun a (op, b) ->
-        match op with
-        | '+' -> a + b
-        | '-' -> a - b
-        | '*' -> a * b
-        | '/' -> a / b
-        | _ -> 0.0) start
-    )
+let pExpr = pipe2 pOperand (many ((pOperator .>> ws) .>>. pOperand)) (fun start ops ->
+    ops |> List.fold (fun a (op, b) -> op a b) start)
 
 do opRef := (braced pExpr <|> pfloat) .>> ws
 
