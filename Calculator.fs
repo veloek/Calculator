5ca4610c2f7a9a03ce394b1a7956327e89a8012e
Vegard Løkken <vegard@loekken.org>
2019-03-26 16:34:37 +0100

    Add modulo operator

--- Calculator.fs
+++ Calculator.fs
@@ -9,8 +9,9 @@ let add = pchar '+' |>> (fun _ a b -> a + b)
 let sub = pchar '-' |>> (fun _ a b -> a - b)
 let mul = pchar '*' |>> (fun _ a b -> a * b)
 let div = pchar '/' |>> (fun _ a b -> a / b)
+let modulo = pchar '%' |>> (fun _ a b -> a % b)
 
-let pOperator = (add <|> sub <|> mul <|> div)
+let pOperator = (add <|> sub <|> mul <|> div <|> modulo)
 let pOperand, opRef = createParserForwardedToRef()
 let pExpr = pipe2 pOperand (many ((pOperator .>> ws) .>>. pOperand)) (fun start ops ->
     ops |> List.fold (fun a (op, b) -> op a b) start)
