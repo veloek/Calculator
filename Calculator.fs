4cff8b9327fca9d47eedae6c35f7e8bf9ae0358d
Vegard Løkken <vegard@loekken.org>
2019-03-26 20:51:43 +0100

    Refactor to make it more readable

--- Calculator.fs
+++ Calculator.fs
@@ -5,13 +5,15 @@ open FParsec
 let ws = spaces
 let braced p = (pchar '(') >>. p .>> (pchar ')')
 
-let add = pchar '+' |>> (fun _ a b -> a + b)
-let sub = pchar '-' |>> (fun _ a b -> a - b)
-let mul = pchar '*' |>> (fun _ a b -> a * b)
-let div = pchar '/' |>> (fun _ a b -> a / b)
-let modulo = pchar '%' |>> (fun _ a b -> a % b)
+let operator op _ = op
 
-let pOperator = (add <|> sub <|> mul <|> div <|> modulo)
+let pAdd = pchar '+' |>> operator (+)
+let pSub = pchar '-' |>> operator (-)
+let pMul = pchar '*' |>> operator (*)
+let pDiv = pchar '/' |>> operator (/)
+let pMod = pchar '%' |>> operator (%)
+
+let pOperator = (pAdd <|> pSub <|> pMul <|> pDiv <|> pMod)
 let pOperand, opRef = createParserForwardedToRef()
 let pExpr = pipe2 pOperand (many ((pOperator .>> ws) .>>. pOperand)) (fun start ops ->
     ops |> List.fold (fun a (op, b) -> op a b) start)
